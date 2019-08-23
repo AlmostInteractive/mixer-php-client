03f78184f74cf503fd3fc85fbf969bfe48545d38
Steven J <3893631+AlmostInteractive@users.noreply.github.com>
2019-08-23 12:03:19 -0500

    Rename 'Client' -> 'Api'

--- test/OauthTest.php
+++ test/OauthTest.php
@@ -1,17 +1,17 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
-use MixerClient\MixerGuzzleClient;
-use MixerClient\MixerClient;
+use MixerApi\MixerGuzzleClient;
+use MixerApi\MixerApi;
 
 class OauthTest extends TestCase
 {
     public function testCanCreateClassWithMinimumOptions()
     {
         $mixerGuzzleClient = new MixerGuzzleClient('CLIENT-ID');
-        $mixerClient = new MixerClient($mixerGuzzleClient, 'CLIENT-ID', 'CLIENT-SECRET');
-        $this->assertInstanceOf(MixerClient::class, $mixerClient);
+        $mixerApi = new MixerApi($mixerGuzzleClient, 'CLIENT-ID', 'CLIENT-SECRET');
+        $this->assertInstanceOf(MixerApi::class, $mixerApi);
 
-        return $mixerClient;
+        return $mixerApi;
     }
 }
